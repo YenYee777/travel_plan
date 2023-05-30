885dc5fb87b772f7c308a108897d470229368b98
YenYee777 <122324810+YenYee777@users.noreply.github.com>
2023-05-30 19:44:29 +0800

    Add files via upload

---
+++ JustGo/JustGo/Controllers/HomeController.cs
@@ -0,0 +1,59 @@
+﻿using JustGo.Models;
+using JustGo.Repository;
+using JustGo.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+using Newtonsoft.Json;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace JustGo.Controllers
+{
+    public class HomeController : BaseController
+    {
+        private readonly ILogger<HomeController> _logger;
+        private readonly IPlaceWeatherRepostiory _pwr;
+        private readonly IScheduleRepostioy _schedule;
+        private readonly IBlogRepostioy _blog;
+
+        public HomeController(ILogger<HomeController> logger, IPlaceWeatherRepostiory pwr, IScheduleRepostioy schedule, IBlogRepostioy blog)
+        {
+            _logger = logger;
+            _pwr = pwr;
+            _schedule = schedule;
+            _blog = blog;           
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Blog()
+        {
+            return View("EditBlog");
+        }
+
+        public IActionResult EditBlog()
+        {
+            return View();
+        }
+
+        public IActionResult itinerary()
+        {
+            return View();
+        }
+
+        public IActionResult UserCatelog()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+
+    }
+
+}
---
+++ JustGo/JustGo/Controllers/ScheduleController.cs
@@ -0,0 +1,77 @@
+﻿using JustGo.Repository;
+using JustGo.ViewModels;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using System.Diagnostics;
+using System.Security.Claims;
+
+namespace JustGo.Controllers
+{
+    public class ScheduleController : BaseController
+    {
+        readonly IPlaceWeatherRepostiory _place;
+        readonly IScheduleRepostioy _schedule;
+        readonly ILogger _logger;
+        public ScheduleController(IScheduleRepostioy schedule, IPlaceWeatherRepostiory place, ILogger<ScheduleController> logger)
+        {
+            _place = place;
+            _schedule = schedule;
+            _logger = logger;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public IActionResult getPlace([FromBody] SelectPlaceVM select)
+        {            
+            return Json(_place.getPlace(select));
+        }
+
+        [HttpPost]
+        public IActionResult selectPlaceFilter([FromBody]SelectPlaceVM vm)
+        {            
+            return Json(_place.getPlaceFilter(vm));
+        }
+
+        //--------------------------------------------------
+        //搜尋使用者行程清單/無細項
+        public IActionResult selectUserSchedule()
+        {
+            //Console.WriteLine(Json(_schedule.selectUserSchedule("862c02ac-67e1-461f-ac15-74b166c0a1e4")));
+            //return Json(_schedule.selectUserSchedule("1"));
+            return Json(_schedule.selectUserSchedule(GetUserId()));
+        }
+
+        //搜尋行程細項
+        [HttpPost]
+        public IActionResult selectDetail([FromBody] ScheduleVM vm)
+        {
+            return Json(_schedule.selectScedule(vm.ScheduleId,GetUserId()));
+        }
+
+        //新增行程
+        [HttpPost]
+        [Authorize]
+        public IActionResult setSchedule([FromBody] ScheduleVM vm)
+        {
+            if (vm.UserId != null)
+            {
+                vm.UserId = GetUserId();
+                return Json(_schedule.editScedule(vm));
+            }
+            vm.UserId = GetUserId();
+            return Json(_schedule.createScedule(vm));
+            //return View("Index");
+
+        }
+
+        //刪除行程
+        [HttpPost]
+        public IActionResult deleteSchedule([FromBody] ScheduleVM vm)
+        {
+            return Json(_schedule.deleteScedule(vm.ScheduleId));
+        }       
+    }
+}
