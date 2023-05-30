d9d8f1b2722dc14008fde6e8d962d8e44679ca22
YenYee777 <122324810+YenYee777@users.noreply.github.com>
2023-05-30 19:47:28 +0800

    Add files via upload

---
+++ JustGo/JustGo/ViewModels/ScheleVM.cs
@@ -0,0 +1,28 @@
+﻿using JustGo.Models;
+namespace JustGo.ViewModels
+{
+    public class ScheduleVM
+    {
+        
+        public int ScheduleId { get; set; }
+        public string? UserId { get; set; }
+        public string? Title { get; set; }
+        public DateTime StartDate { get; set; }
+        public DateTime EndDate { get; set; }
+        public bool? WeatherWarning { get; set; }
+
+        public IList<IList<ScheduleDetailVM>>? Details { get; set; }
+    }
+    public class ScheduleDetailVM
+    {
+        public DateTime StartTime { get; set; }
+        public DateTime EndTime { get; set; }        
+        public bool? WeatherWarning { get; set; }
+        public int? Pop { get; set; }
+        public int? Temperature { get; set; }
+        public int? Uvi { get; set; }
+
+        public Place? Place { get; set; }
+    }
+
+}
